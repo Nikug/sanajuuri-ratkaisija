887d3f7adf38883e2e7cf6d1b3b100da10bdf497
niku <niku.gronberg@develore.com>
2025-07-03 14:11:12 +0300

    find all solutions

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
 import fs from "fs";
 import { TrieTree } from "tree";
 
+interface Solution {
+  words: string[];
+  remainingChars: string[];
+}
+
 const loadWords = (): TrieTree => {
   const json = fs.readFileSync("scripts/words.json", "utf8");
   const words = JSON.parse(json);
@@ -10,20 +15,56 @@ const loadWords = (): TrieTree => {
   return tree;
 };
 
+const solve = (tree: TrieTree, usedChars: string[], remainingChars: string[]) => {
+  const stack: Solution[] = [{ words: [usedChars.join("")], remainingChars }];
+  const solutions: Solution[] = [];
+  const allChars = [...usedChars, ...remainingChars];
+
+  while (stack.length > 0) {
+    const currentSolution = stack.pop()!;
+    const words = tree.search(
+      currentSolution.words.at(-1)!.split(""),
+      currentSolution.remainingChars
+    );
+
+    for (const word of words) {
+      // TODO: find a better way to get new remaining chars
+      const newRemainingChars = [...allChars];
+      word
+        .split("")
+        .forEach((char) => newRemainingChars.splice(newRemainingChars.indexOf(char), 1));
+
+      const nextSolution = {
+        words: [...currentSolution.words, word],
+        remainingChars: newRemainingChars,
+      };
+
+      if (nextSolution.remainingChars.length === 0) {
+        solutions.push(nextSolution);
+      } else {
+        stack.push(nextSolution);
+      }
+    }
+  }
+
+  return solutions;
+};
+
 const main = () => {
   console.log("Loading words...");
   console.time("Load");
   const tree = loadWords();
   console.timeEnd("Load");
 
-  console.time("Word find");
+  const usedChars = "osa".split("");
+  const remainingChars = "taesl".split("");
 
-  const usedChars = "osat".split("");
-  const remainingChars = "aesl".split("");
-  const foundWords = tree.search(usedChars, remainingChars);
-  console.timeEnd("Word find");
+  console.log("Solving...");
+  console.time("Solve");
+  const solutions = solve(tree, usedChars, remainingChars);
+  console.timeEnd("Solve");
 
-  console.log(foundWords);
+  console.log("Found solutions:", solutions.length);
 };
 
 main();
