1d1159b04d26d2e40cd36c73250955c399a46289
niku <niku.gronberg@develore.com>
2025-07-03 13:11:58 +0300

    add tree logic

--- main.ts
+++ main.ts
@@ -1,5 +1,26 @@
+import fs from "fs";
+import { TrieTree } from "tree";
+
+const loadWords = (): TrieTree => {
+  const json = fs.readFileSync("scripts/words.json", "utf8");
+  const words = JSON.parse(json);
+
+  const tree = new TrieTree();
+  tree.insertMany(words);
+  return tree;
+};
+
 const main = () => {
-  console.log("hello");
+  console.log("Loading words...");
+  console.time("Load");
+  const tree = loadWords();
+  console.timeEnd("Load");
+
+  console.time("Word find");
+  const foundWords = tree.search("test", 7);
+  console.timeEnd("Word find");
+
+  console.log(foundWords);
 };
 
 main();
--- tree.ts
+++ tree.ts
@@ -1,8 +1,82 @@
 export interface TreeNode {
   character: string;
   nodes: TreeNode[];
+  isWord: boolean;
 }
 
 const characters = "abcdefghijklmnopqrstuvwxyzåäö";
 
-export class TrieTree {}
+export class TrieTree {
+  private root: TreeNode = { character: "", nodes: [], isWord: false };
+
+  public insertMany(words: string[]): void {
+    for (const word of words) {
+      this.insert(word);
+    }
+  }
+
+  public insert(word: string): void {
+    let node = this.root;
+
+    for (let i = 0; i < word.length; i++) {
+      const char = word[i];
+      const index = characters.indexOf(char);
+      if (node.nodes[index]) {
+        node = node.nodes[index];
+      } else {
+        node.nodes[index] = { character: char, nodes: [], isWord: i === word.length - 1 };
+        node = node.nodes[index];
+      }
+    }
+  }
+
+  public search(word: string, wordLength: number, resultLimit: number = -1): string[] {
+    const chars = word.split("");
+    let node = this.root;
+    const result: string[] = [];
+
+    for (const char of chars) {
+      const index = characters.indexOf(char);
+      if (!node.nodes[index]) return result;
+      node = node.nodes[index];
+    }
+
+    this.walk(node, chars.slice(0, -1), result, wordLength, resultLimit);
+
+    return result;
+  }
+
+  private walk(
+    node: TreeNode,
+    path: string[],
+    results: string[],
+    wordLength: number,
+    resultLimit: number
+  ): void {
+    // Base case
+    if (
+      !node ||
+      (resultLimit !== -1 && results.length >= resultLimit) ||
+      path.length > wordLength
+    ) {
+      return;
+    }
+
+    // Pre recurse
+    path.push(node.character);
+
+    if (node.isWord && path.length === wordLength) {
+      results.push(path.join(""));
+    }
+
+    // Recurse
+    for (const child of node.nodes) {
+      if (child) {
+        this.walk(child, path, results, wordLength, resultLimit);
+      }
+    }
+
+    // Post recurse
+    path.pop();
+  }
+}
