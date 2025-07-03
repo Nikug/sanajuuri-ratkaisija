370e783ac7d35b86de2f8a4eb6cc90ccd83a3fbb
niku <niku.gronberg@develore.com>
2025-07-03 14:46:10 +0300

    ask for user input

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 import fs from "fs";
 import { TrieTree } from "tree";
+import { createInterface } from "readline/promises";
+import { stdin, stdout } from "process";
 
 interface Solution {
   words: string[];
@@ -16,6 +18,24 @@ const loadWords = (): TrieTree => {
   return tree;
 };
 
+const prettyPrintSolution = (words: string[]): void => {
+  if (words.length === 0) return;
+  const longestWord = words.at(-1)!.length;
+
+  console.log();
+  for (const word of words) {
+    const spaces = longestWord - word.length;
+    console.log(" ".repeat(spaces) + word.split("").join(" "));
+  }
+};
+
+const listSubtract = (original: string[], sub: string[]): string[] => {
+  const copy = [...original];
+  sub.forEach((char) => copy.splice(copy.indexOf(char), 1));
+
+  return copy;
+};
+
 const solve = (tree: TrieTree, usedChars: string[], remainingChars: string[]) => {
   const solutions: Solution[] = [];
   const allChars = [...usedChars, ...remainingChars];
@@ -26,9 +46,8 @@ const solve = (tree: TrieTree, usedChars: string[], remainingChars: string[]) =>
     const words = tree.search(currentSolution.usedChars, currentSolution.remainingChars);
 
     for (const word of words) {
-      const newRemainingChars = [...allChars];
       const newUsedChars = word.split("");
-      newUsedChars.forEach((char) => newRemainingChars.splice(newRemainingChars.indexOf(char), 1));
+      const newRemainingChars = listSubtract(allChars, newUsedChars);
 
       const nextSolution = {
         words: [...currentSolution.words, word],
@@ -47,14 +66,19 @@ const solve = (tree: TrieTree, usedChars: string[], remainingChars: string[]) =>
   return solutions;
 };
 
-const main = () => {
+const main = async () => {
   console.log("Loading words...");
   console.time("Load");
   const tree = loadWords();
   console.timeEnd("Load");
 
-  const usedChars = "osa".split("");
-  const remainingChars = "taesl".split("");
+  const rl = createInterface({ input: stdin, output: stdout });
+  const firstWord = await rl.question("First word: ");
+  const allChars = await rl.question("All characters: ");
+  rl.close();
+
+  const usedChars = firstWord.split("");
+  const remainingChars = listSubtract(allChars.split(""), usedChars);
 
   console.log("Solving...");
   console.time("Solve");
@@ -62,6 +86,11 @@ const main = () => {
   console.timeEnd("Solve");
 
   console.log("Found solutions:", solutions.length);
+
+  if (solutions.length > 0) {
+    console.log("First three solutions:");
+    solutions.slice(0, 3).forEach((solution) => prettyPrintSolution(solution.words));
+  }
 };
 
-main();
+await main();
