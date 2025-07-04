45096d83711914b8102b0a4ccb539d7a17d3fed6
niku <niku.gronberg@develore.com>
2025-07-04 12:28:05 +0300

    box character styles

--- web/src/components/BoxCharacter.tsx
+++ web/src/components/BoxCharacter.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export const BoxCharacter: Component<Props> = (props) => {
   return (
-    <div class="w-8 h-8 border-2 rounded-lg bg-violet-200 font-bold flex justify-center items-center text-xl">
+    <div class="w-10 h-10 border-2 rounded-lg bg-violet-200 font-bold flex justify-center items-center text-xl">
       {props.character.toUpperCase()}
     </div>
   );
